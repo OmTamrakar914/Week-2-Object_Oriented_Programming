b8a46e442e9c4b2449681948e079020326875000
OmTamrakar914 <149686955+OmTamrakar914@users.noreply.github.com>
2025-01-20 12:14:18 +0530

    Created Class for University management system

--- level-1/Access Modifiers/UniversityManagementSystem1.java
+++ level-1/Access Modifiers/UniversityManagementSystem1.java
@@ -1,6 +1,6 @@
-
 import java.util.Scanner;
 
+// Created class for University Managemnet 
 public class UniversityManagementSystem1{
 	public static void main(String[]args){
 		Scanner sc = new Scanner(System.in);
